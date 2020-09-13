284f1ed41611879ae45a07ce4d0057a2ce2d1a75
zcook04 <zacharyjcook@outlook.com>
2020-09-13 08:54:40 -0400

    Filereader section complete

---
+++ Typescript Complete Developers Guide/Stats/build/index.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var MatchReader_1 = require("./MatchReader");
+var MatchResult_1 = require("./MatchResult");
+var CsvFileReader_1 = require("./CsvFileReader");
+var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+var manUnitedWins = 0;
+for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+    else {
+        continue;
+    }
+}
+console.log("Man United won " + manUnitedWins + " games");
---
+++ Typescript Complete Developers Guide/Stats/src/index.ts
@@ -0,0 +1,22 @@
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
+
+const csvFileReader = new CsvFileReader('football.csv');
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+let manUnitedWins = 0;
+
+for (let match of matchReader.matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  } else {
+    continue;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
