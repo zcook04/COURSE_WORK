6a8a017058d47be0806ad3cf53ed4f94a56a490e
Zack <zcook04@gmail.com>
2020-12-30 22:50:25 -0500

    Typescript Developers Guide WebFramework Refactor

---
+++ Typescript Complete Developers Guide/web/src/models/User.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosResponse } from "axios";
+import { Eventing } from './Eventing'
+
+interface UserProps {
+    id?: number,
+    name?: string,
+    age?: number
+}
+
+export class User {
+
+    constructor(private data: UserProps) {}
+
+    get(propName: string): (string | number) {
+        return this.data[propName]
+    }
+
+    set(update: UserProps): void {
+        Object.assign(this.data, update);
+    }
+
+
+
+    fetch(): void {
+        axios.get(`http://localhost:3000/users/${this.get('id')}`)
+        .then((response: AxiosResponse): void => {
+            this.set(response.data)
+        })
+    }
+
+    save(): void {
+        const id = this.get('id')
+        
+        if (id) {
+            axios.put(`http://localhost:3000/users/${id}`, this.data)
+        } else {
+            axios.post("http://localhost:3000/users", this.data)
+        }
+
+    }
+}
